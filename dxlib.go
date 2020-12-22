af242556fc8c93d1d386888ee469550565814b74
s.miyoshi <s.miyoshi1991@gmail.com>
2020-12-22 21:23:28 +0900

    add init, end function

---
+++ dxlib.go
@@ -0,0 +1,7 @@
+package dxlib
+
+//go:generate go run cmd/generate.go -i dxlib.go -o dxlib_gen.go
+//go:generate gofmt -w dxlib_gen.go
+
+//dxlib func DxLib_Init() int "dx_DxLib_Init"
+//dxlib func DxLib_End() int "dx_DxLib_End"
