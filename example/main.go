ca5825ec0296265346884107aa8c4522f6475f6f
s.miyoshi <s.miyoshi1991@gmail.com>
2021-01-15 23:47:18 +0900

    add comment to example code

--- example/main.go
+++ example/main.go
@@ -6,16 +6,20 @@ import (
 	"github.com/sh-miyoshi/dxlib"
 )
 
+// All calls to DX Library must happen from the same thread
+// that creates the DX Library object so make sure to add
+// this code in your main package:
 func init() {
 	runtime.LockOSThread()
 }
 
 func main() {
+	// Initialize dxlib functions with DxLib.dll
 	dxlib.Init("DxLib.dll")
 
-	dxlib.ChangeWindowMode(dxlib.TRUE)
-	dxlib.SetGraphMode(640, 480, 16, 60)
-	dxlib.SetOutApplicationLogValidFlag(dxlib.TRUE)
+	dxlib.ChangeWindowMode(dxlib.TRUE)              // Run as window mode
+	dxlib.SetGraphMode(640, 480, 16, 60)            // Set screen size: 640 * 480, 16bit, 60 FPS
+	dxlib.SetOutApplicationLogValidFlag(dxlib.TRUE) // Output DxLib log
 
 	dxlib.DxLib_Init()
 	dxlib.SetDrawScreen(dxlib.DX_SCREEN_BACK)
