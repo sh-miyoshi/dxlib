04d5c263b169a010608edbf5caea6dfa21202309
s.miyoshi <s.miyoshi1991@gmail.com>
2021-01-30 19:32:02 +0900

    remove comment

--- cmd/generate/main.go
+++ cmd/generate/main.go
@@ -9,39 +9,6 @@ import (
 	"github.com/sh-miyoshi/dxlib/cmd/generate/parse"
 )
 
-// func parseComment(line string) error {
-// 	if !strings.HasPrefix(line, "//comment") {
-// 		return nil
-// 	}
-
-// 	// valid format
-// 	//comment; <FuncName>; <comments>
-// 	data := strings.Split(line, ";")
-// 	if len(data) != 3 {
-// 		return fmt.Errorf("invalid comment format")
-// 	}
-
-// 	comments = append(comments, comment{
-// 		funcName: strings.TrimSpace(data[1]),
-// 		comments: strings.Split(strings.TrimSpace(data[2]), "\\n"),
-// 	})
-
-// 	return nil
-// }
-
-// func getComment(funcName string) string {
-// 	for _, c := range comments {
-// 		if c.funcName == funcName {
-// 			res := "// " + funcName + " "
-// 			for _, msg := range c.comments {
-// 				res += msg + "\n// "
-// 			}
-// 			return strings.TrimSuffix(res, "// ")
-// 		}
-// 	}
-// 	return ""
-// }
-
 func main() {
 	var input, out string
 	flag.StringVar(&input, "input", "", "input file name")
@@ -70,28 +37,4 @@ func main() {
 	}
 
 	output.Output(out, *data)
-
-	// 	// Parse function
-	// 	d, err := parse(line)
-	// 	if err != nil {
-	// 		fmt.Printf("Failed to parse line %s: %v\n", line, err)
-	// 		os.Exit(1)
-	// 	}
-	// 	if d != nil {
-	// 		data = append(data, *d)
-	// 		continue
-	// 	}
-
-	// 	// Parse extra function
-	// 	if d := parseExtraFunc(line); d != nil {
-	// 		extra = append(extra, *d)
-	// 		continue
-	// 	}
-
-	// 	// Parse command
-	// 	if err := parseComment(line); err != nil {
-	// 		fmt.Printf("Failed to parse line %s: %v\n", line, err)
-	// 		os.Exit(1)
-	// 	}
-	// }
 }
